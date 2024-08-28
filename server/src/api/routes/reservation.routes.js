0a613d4ca6b90977f3ed28cb0f03ece8f2b98c5f
monicapenacho <monicapenachocollado@gmail.com>
2024-08-28 15:25:46 +0200

    reservation endpoint controllers y rutas creadas

---
+++ server/src/api/routes/reservation.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { getAllReservations, getReservationbyId, createReservation, deleteReservation, updateReservation } = require('../controllers/philo.controller');
+const upload = require('../../middleware/upload');
+
+const routeReservations = express.Router();
+
+routeReservations.get('/all', getAllReservations);
+routeReservations.get('/:id', getReservationbyId);
+routeReservations.post('/new', upload.single('photo'), createReservation);
+routeReservations.delete('/delete/:id', deleteReservation);
+routeReservations.put('/update/:id', updateReservation);
+
+module.exports = routeReservations;
