a1d671fc466868b18991b9122269db8bc2da10b4
monicapenacho <monicapenachocollado@gmail.com>
2024-08-29 15:02:32 +0200

    export functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Router from './routes/Router';
+import Login from './pages/Login';
 
 
 function App() {
   return (
     <div>
       <Router />
+  
     </div>
   );
 }
