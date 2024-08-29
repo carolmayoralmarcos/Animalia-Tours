b7d22de4c528cba1d56b6e15098770058699525e
Carolina <caroolmayoral@gmail.com>
2024-08-29 14:45:20 +0200

    creacion funciones paginas

---
+++ client/src/pages/ActivityById.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ActivityById() {
+    return (
+        <div className="container content">
+            <h1>Activity By ID</h1>
+        </div>
+    );
+}
+
+export default ActivityById;
