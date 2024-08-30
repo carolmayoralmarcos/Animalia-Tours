0c798ad39b6ed2ba33ba136a3037144cc60a6dff
Haritz Bechet <haritz.bechet@gmail.com>
2024-08-30 11:39:41 +0200

    CartProvider added to Route + deleteElement util

--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -22,33 +22,36 @@ import Register from '../pages/Register';
 import Cart from '../pages/Cart';
 import Contact from '../pages/Contact';
 import About from '../pages/About';
+import { CartProvider } from '../context/CartContext';
 
 function Router() {
 
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout><Home /></Layout>} />
-                <Route path="/activities" element={<Layout><Activities /></Layout>} />
-                <Route path="/cities" element={<Layout><Cities /></Layout>} />
-                <Route path="/view/:collection/:id" element={<Layout><ViewElement /></Layout>} />
-                {/* <Route path="/view/activity/:id" element={<Layout><ViewActivity /></Layout>} />
+        <CartProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Layout><Home /></Layout>} />
+                    <Route path="/activities" element={<Layout><Activities /></Layout>} />
+                    <Route path="/cities" element={<Layout><Cities /></Layout>} />
+                    <Route path="/view/:collection/:id" element={<Layout><ViewElement /></Layout>} />
+                    {/* <Route path="/view/activity/:id" element={<Layout><ViewActivity /></Layout>} />
                 <Route path="/view/city/:id" element={<Layout><ViewCity /></Layout>} /> */}
-                {/* <Route path="/update/:collection/:id" element={<PrivateRoute role="client"><Layout><UpdateElement /></Layout></PrivateRoute>} /> */}
-                <Route path="/update/activities/:id" element={<Layout><UpdateActivity /></Layout>} />
-                <Route path="/update/cities/:id" element={<Layout><UpdateCity /></Layout>} />
-                <Route path="/new/city" element={<Layout><NewCity /></Layout>} />
-                <Route path="/new/activity" element={<Layout><NewActivity /></Layout>} />
-                <Route path="/register" element={<Layout><Register /></Layout>} />
-                <Route path="/login" element={<Layout><Login /></Layout>} />
-                <Route path="/profile" element={<Layout><UserProfile /></Layout>} />
-                {/* <Route path="/users" element={<Layout><UsersPage /></Layout>} /> */}
-                <Route path="/users" element={<Layout><Cart /></Layout>} />
-                <Route path="/contact" element={<Layout><Contact /></Layout>} />
-                <Route path="/about" element={<Layout><About /></Layout>} />
-                <Route path="*" element={<Layout><NotFound /></Layout>} />
-            </Routes>
-        </BrowserRouter>
+                    {/* <Route path="/update/:collection/:id" element={<PrivateRoute role="client"><Layout><UpdateElement /></Layout></PrivateRoute>} /> */}
+                    <Route path="/update/activities/:id" element={<Layout><UpdateActivity /></Layout>} />
+                    <Route path="/update/cities/:id" element={<Layout><UpdateCity /></Layout>} />
+                    <Route path="/new/city" element={<Layout><NewCity /></Layout>} />
+                    <Route path="/new/activity" element={<Layout><NewActivity /></Layout>} />
+                    <Route path="/register" element={<Layout><Register /></Layout>} />
+                    <Route path="/login" element={<Layout><Login /></Layout>} />
+                    <Route path="/profile" element={<Layout><UserProfile /></Layout>} />
+                    {/* <Route path="/users" element={<Layout><UsersPage /></Layout>} /> */}
+                    <Route path="/users" element={<Layout><Cart /></Layout>} />
+                    <Route path="/contact" element={<Layout><Contact /></Layout>} />
+                    <Route path="/about" element={<Layout><About /></Layout>} />
+                    <Route path="*" element={<Layout><NotFound /></Layout>} />
+                </Routes>
+            </BrowserRouter>
+        </CartProvider>
     );
 }
 
