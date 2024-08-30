af3fa63817538cb232a5ff7c88ecb331dbe463e5
monicapenacho <monicapenachocollado@gmail.com>
2024-08-30 17:11:38 +0200

    Cart: button created confirmation = new reservation confirmed

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -3,12 +3,55 @@ import "../App.css";
 import { FaShoppingCart, FaTrashAlt } from "react-icons/fa"; 
 import CalculateTotal from "../components/CalculateTotal";
 import { CartContext } from "../context/CartContext";      
-
+import { useNavigate } from "react-router-dom"; 
 
 
 export default function Cart() {
 
-  const { cart, addToCart, removeFromCart } = useContext(CartContext);
+  const { cart, removeFromCart } = useContext(CartContext);
+  
+  const navigate = useNavigate();
+
+  const handleConfirmReservations = () => {
+    
+    const userId = localStorage.getItem("userId");
+
+    if (!userId) {
+      console.error("No user ID found. Please log in.");
+      return; 
+    }
+
+    cart.forEach((item) => {
+      const reservationData = {
+        name: `Reservation for ${item.name}`,
+        status: "confirmed",
+        user: userId,
+        activity: item._id
+      };
+
+      fetch("http://localhost:5000/api/reservations", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(reservationData),
+      })
+      .then(response => response.json())
+      .then(data => {
+        if (data.success) {
+          console.log("Reservation created:", data.data);
+        } else {
+          console.error("Error creating reservation:", data.data);
+        }
+      })
+      .catch(err => {
+        console.error("Error with reservation request:", err);
+      });
+    });
+
+    
+    navigate("/profile"); 
+  };
    
   return (
     <div className="cart">
@@ -29,6 +72,9 @@ export default function Cart() {
         ))}
       </ul>
         <CalculateTotal cart={cart}/>
+        <button className="btn btn-primary" onClick={handleConfirmReservations}>
+        Reserva Confirmada
+      </button>
   </div>
   );
 }
