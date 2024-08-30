e55726be86b8d337274ae285f1cef4a3e6c1c6da
Aleida <aleida1983@hotmail.com>
2024-08-30 15:59:51 +0200

    navbar funcionando

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,7 @@ import Logo from "../Animaliatour.png"
 function Header() {
     return (
         <Navbar bg="info p-2 text-dark bg-opacity-25" expand="lg">
-            <Navbar.Brand href="#home">
+            <Navbar.Brand href="/home">
                 <img
                     src={Logo}
                     width="80"
@@ -19,14 +19,14 @@ function Header() {
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mx-auto">
                     <Nav.Link href="/">Inicio</Nav.Link>
-                    <Nav.Link href="#About">Sobre Nosotros</Nav.Link>
-                    <Nav.Link href="#Activities">Actividades</Nav.Link>
-                    <Nav.Link href="#Cities">Ciudades</Nav.Link>
-                    <Nav.Link href="#contact">Contacto</Nav.Link>
-                    <Nav.Link href="#Register">Registro</Nav.Link>
-                    <Nav.Link href="#Login">Login</Nav.Link>
-                    <Nav.Link href="#UserProfile">Perfil</Nav.Link>
-                    <Nav.Link href="#Cart">Carrito</Nav.Link>
+                    <Nav.Link href="/About">Sobre Nosotros</Nav.Link>
+                    <Nav.Link href="/Activities">Actividades</Nav.Link>
+                    <Nav.Link href="/Cities">Ciudades</Nav.Link>
+                    <Nav.Link href="/contact">Contacto</Nav.Link>
+                    <Nav.Link href="/Register">Registro</Nav.Link>
+                    <Nav.Link href="/Login">Login</Nav.Link>
+                    <Nav.Link href="/UserProfile">Perfil</Nav.Link>
+                    <Nav.Link href="/Cart">Carrito</Nav.Link>
 
                 </Nav>
             </Navbar.Collapse>
