ae6a4780b3f1710edc531a0deccfec6d1bcff94d
Aleida <aleida1983@hotmail.com>
2024-08-31 12:23:53 +0200

    navbar arreglado

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import Logo from "../Animaliatour.png"
 import '../styles/styles.css'
-
+import { Link } from 'react-router-dom'
 
 
 
@@ -22,15 +22,15 @@ function Header() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mx-auto">
-                    <Nav.Link href="/">Inicio</Nav.Link>
-                    <Nav.Link href="/about">Sobre Nosotros</Nav.Link>
-                    <Nav.Link href="/activities">Actividades</Nav.Link>
-                    <Nav.Link href="/cities">Ciudades</Nav.Link>
-                    <Nav.Link href="/contact">Contacto</Nav.Link>
-                    <Nav.Link href="/register">Registro</Nav.Link>
-                    <Nav.Link href="/login">Login</Nav.Link>
-                    <Nav.Link href="/userProfile">Perfil</Nav.Link>
-                    <Nav.Link href="/cart">Carrito</Nav.Link>
+                    <Nav.Link as={Link} to="/">Inicio</Nav.Link>
+                    <Nav.Link as={Link} to="/about">Sobre Nosotros</Nav.Link>
+                    <Nav.Link as={Link} to="/activities">Actividades</Nav.Link>
+                    <Nav.Link as={Link} to="/cities">Ciudades</Nav.Link>
+                    <Nav.Link as={Link} to="/contact">Contacto</Nav.Link>
+                    <Nav.Link as={Link} to="/register">Registro</Nav.Link>
+                    <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                    <Nav.Link as={Link} to="/userProfile">Perfil</Nav.Link>
+                    <Nav.Link as={Link} to="/cart">Carrito</Nav.Link>
 
                 </Nav>
             </Navbar.Collapse>
