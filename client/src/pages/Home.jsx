3d575baacf24a54cf752e21b743b261230e6418c
Aleida <aleida1983@hotmail.com>
2024-08-31 14:53:37 +0200

    hero en home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import CarouselInterval from '../components/Hero';
 
 function Home() {
     return (
         <div className='container content'>
             <h1>Home</h1>
+            <CarouselInterval />
         </div>
     );
 }
