8bf780cdd3d217d51c758d21763bb1d2c1397ca7
monicapenacho <monicapenachocollado@gmail.com>
2024-09-02 12:28:58 +0200

    Deleted Carousel Aleatorio & utils Randmom

--- client/src/utils/getrandomactivities.js
+++
@@ -1,5 +0,0 @@
-export function getRandomActivities(activityArray, count) {
-    const shuffledActivities = activityArray.sort(() => 0.5 - Math.random());
-    return shuffledActivities.slice(0, count);
-  }
-  
