0b59a702262d2f26515d45930439cc7be342456b
monicapenacho <monicapenachocollado@gmail.com>
2024-09-03 11:08:17 +0200

    pages: UpdateActivity: edit date before update

--- client/src/pages/UpdateActivity.jsx
+++ client/src/pages/UpdateActivity.jsx
@@ -27,11 +27,11 @@ const UpdateActivity = () => {
                 try {
                     const info = await getElementbyId(id, collection);
                     if (info && info.data) {
-                        const { _v, ...rest } = info.data;
-                        const cityId = info.data.city_id ? info.data.city_id._id : ''; // Extraer solo el ID de la ciudad
+                        const { _v, date, ...rest } = info.data;
+                        const formattedDate = date ? new Date(date).toISOString().split('T')[0] : '';
+                        const cityId = info.data.city_id ? info.data.city_id._id : ''; 
                         console.log("verificar los datos de rest", rest)
-                        // setActivityData(rest);
-                        setActivityData({ ...rest, city_id: cityId });
+                        setActivityData({ ...rest, date: formattedDate, city_id: cityId });
                     }
                 } catch (error) {
                     console.error(`Could not get data: ${error}`);
@@ -192,7 +192,6 @@ const UpdateActivity = () => {
                     <label>Ciudad</label>
                     <select
                         name="city_id"
-                        // value={updatedActivity.city_id || (cities.length > 0 ? cities[0]._id : "")}
                         value={updatedActivity.city_id || ''}
                         onChange={handleChange}
                         className="form-select"
